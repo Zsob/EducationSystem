44136cb86019ad8b941c4aa943420ce32ad3a8cc
极涵 曾 <747039543@qq.com>
2020-11-03 08:46:37 +0800

    1

--- Education System/SystemForm.cs
+++ Education System/SystemForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Education_System
 {
     public partial class Form_System : Form
---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE EducationSystemBase		
+	ON		
+		(NAME='DataFile'	
+		,FILENAME='D:\EducationSystem\EducationSystemBase\DataFile.mdf')	
+	LOG ON		
+		(NAME='LogFile'	
+		,FILENAME='D:\EducationSystem\EducationSystemBase\LogFile.ldf');	
+
+CREATE TABLE tb_Student	
+	(No
+		CHAR(10)
+		NOT NULL
+		CONSTRAINT pk_Student_No
+			PRIMARY KEY(No)
+		CONSTRAINT ck_Student_No	
+			CHECK(No LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+	,Password
+		VARCHAR(20)
+		NOT NULL
+	);
+
+INSERT dbo.tb_Student
+(
+    No,
+    Password
+)
+VALUES
+(   '3190707011', -- No - char(10)
+    '11'  -- Password - varchar(20)
+    )
